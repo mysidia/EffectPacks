57f08e10959ed1945f212ea6d91b3afe6df747d7
Mysidia <69487+mysidia@users.noreply.github.com>
2018-12-19 15:21:07 -0800

    further work on button swap/dpad invert

--- EffectPacks/DKC1.cs
+++ EffectPacks/DKC1.cs
@@ -19,6 +19,60 @@ namespace EffectPacks
         public override Game Game { get; }
             = new Game(0, "Game name Goes Here");
 
+        private enum PadState
+        {
+            NORMAL,
+            INVERT,
+            ROTATE,
+        };
+        private PadState mydpadstate = PadState.NORMAL;
+        private PadState myabxystate = PadState.NORMAL;
+
+        byte[] GetControlPattern(PadState abxy, PadState dpad)
+        {
+            byte[] result = new byte[16];
+
+            
+            // Connector?.WriteBytes(0x7e70f0, new byte[] { 0x33, 0x33, 
+            // 0x00, 0x00,
+            // 0x00, 0x00, 
+            // 0xa0, 0x2a, <- RS1
+            // 0x50, 0xd5, <- LS1
+            // 0x00,  0x00, <- LS2
+            // 0x00, 0x00, <- RS2
+            // 0x0f, 0xc0 <- CP } );
+
+            if (dpad != PadState.NORMAL || abxy != PadState.NORMAL)
+            {
+                result[0] = 0x33; /* Tell the  ROM patch to turn on */
+                result[1] = 0x33;
+            }
+
+            switch (dpad)
+            {
+                default: break;
+                case PadState.INVERT:  result[7] |=  0x0A; result[9] |=  0x05; break;
+                case PadState.ROTATE:  result[11] |= 0x30; result[13] |= 0x03; break;
+            }
+
+            switch(abxy)
+            {
+                default: break;
+                case PadState.INVERT:
+                case PadState.ROTATE:
+                    result[7] |= 0xA0;
+                    result[6]  |= 0x80;
+                    result[9]  |= 0x50;
+                    result[8]  |= 0x40;
+                    break;
+            }
+
+            result[14] = (byte)~(result[4] | result[6] | result[8] | result[10] | result[12]);
+            result[15] = (byte)~(result[5] | result[7] | result[9] | result[11] | result[13]);
+            Connector.SendMessage("result = " + result.ToString());
+            return result;
+        }
+
 
         public DKC1([NotNull] IPlayer player, [NotNull] Func<CrowdControlBlock, bool> responseHandler, [NotNull] Action<object> statusUpdateHandler) : base(responseHandler, statusUpdateHandler) => _player = player;
         public override List<Effect> Effects => new List<Effect>(new[]
@@ -27,7 +81,10 @@ namespace EffectPacks
             new Effect("Take Bananas", "takebananas", 1),
             new Effect("Send extra life", "extralife", 1),
             new Effect("Darkness (26 seconds)", "darkness", 1),
-            new Effect("Lift", "lift", 1)
+            new Effect("Lift", "lift", 1),
+            new Effect("Reverse D-Pad (1 minute)", "invertdpad", 1),
+            new Effect("Rotate D-Pad (1 minute)", "rotatedpad", 1),
+            new Effect("Swap buttons (1 minute)", "swapbuttons", 1),
         });
 
         public override List<(string, Action)> MenuActions
@@ -57,6 +114,16 @@ namespace EffectPacks
 
                 DelayEffect(request, TimeSpan.FromSeconds(10));
                 return;
+            }
+
+            if ( mydpadstate != PadState.NORMAL )
+            {
+                if (request.InventoryItem.BaseItem.Code.Equals("invertdpad") ||
+                    request.InventoryItem.BaseItem.Code.Equals("reversedpad"))
+                {
+                    DelayEffect(request, TimeSpan.FromSeconds(5));
+                    return;
+                }
             }
 
             if (request.InventoryItem.BaseItem.Code.Equals("darkness"))
@@ -163,6 +230,42 @@ namespace EffectPacks
                         Respond(request, success, message);
                         return;
                     }
+                case "invertdpad":
+                    {
+                        if ( Connector?.WriteBytes(0x7e70f0,
+                             GetControlPattern(myabxystate, PadState.INVERT)) ?? false )
+                        {
+                            mydpadstate = PadState.INVERT;
+                            var (success, message) = StartTimed(request, 0x7e70f0, 0x33, TimeSpan.FromMinutes(1), "reversed your dpad");
+                            Respond(request, success ?? false, message);
+                        }
+                        Respond(request, false, "could not invert dpad");
+                        return;
+                    }
+                case "rotatedpad":
+                    {
+                        if (Connector?.WriteBytes(0x7e70f0,
+                             GetControlPattern(myabxystate, PadState.ROTATE)) ?? false)
+                        {
+                            mydpadstate = PadState.ROTATE;
+                            var (success, message) = StartTimed(request, 0x7e70f0, 0x33, TimeSpan.FromMinutes(1), "rotated your dpad");
+                            Respond(request, success ?? false, message);
+                        }
+                        Respond(request, false, "could not rotate dpad");
+                        return;
+                    }
+                case "swapbuttons":
+                    {
+                        if (Connector?.WriteBytes(0x7e70f0,
+                             GetControlPattern(PadState.INVERT, mydpadstate)) ?? false)
+                        {
+                            myabxystate = PadState.INVERT;
+                            var (success, message) = StartTimed(request, 0x7e70f0, 0x33, TimeSpan.FromMinutes(1), "swapped the buttons around");
+                            Respond(request, success ?? false, message);
+                        }
+                        Respond(request, false, "could not swap the buttons around");
+                        return;
+                    }
             }
         }
 
@@ -191,8 +294,32 @@ namespace EffectPacks
                         }
                         return result;
                     }
-            }
 
+                case "invertdpad":
+                case "rotatedpad":
+                    {
+                        if (Connector?.WriteBytes(0x7e70f0,
+                             GetControlPattern(myabxystate, PadState.NORMAL)) ?? false)
+                        {
+                            mydpadstate = PadState.NORMAL;
+                            Connector?.SendMessage($"{request.DisplayViewer}'s dpad affect has ended.");
+                            return true;
+                        }
+                        return false;
+                    }
+                case "swapbuttons":
+                    {
+                        if (Connector?.WriteBytes(0x7e70f0,
+                             GetControlPattern(PadState.NORMAL, mydpadstate)) ?? false)
+                        {
+                            myabxystate = PadState.NORMAL;
+                            Connector?.SendMessage($"{request.DisplayViewer}'s button swap has ended.");
+                            return true;
+                        }
+                        return false;
+
+                    }
+            }
             return false;
         }
 
@@ -209,6 +336,9 @@ namespace EffectPacks
 
                     success &= Connector?.WriteBytes(0x7e1b0c, new byte[] { 0x00, 0x00 }) ?? false;
                     success &= Connector?.WriteByte(0x7e051b, 0x7f) ?? false;//
+                    success &= Connector?.WriteByte(0x7e70f0, 0x00) ?? false;
+                    mydpadstate = PadState.NORMAL;
+                    myabxystate = PadState.NORMAL;
                 }
                 catch
                 {
@@ -314,16 +444,14 @@ at 0081:7F80 change to [8A] - txa
 
 Custom ASM to provide control of Joypad 1 Inputs
 
-0081:7F80 8A8D0205AF00707EC91A1A
-0081:7F8B D04A9C0270
-0081:7F90 AF04707E 4D0005 0c0270
-0081:79FA AF06707E 2d0005 4A 0c0270
-0081:7FA5 AF08707E 2d0005 0a 0c0270
-0081:7Fb0 AF0A707E 2d0005 4a 0c0270 af0c707e 2d
-0081:7fc0 0005 0a 0c0270 af0e707e 2d0005 0c0270
-0081:7fd0 ad0270 8d0005 6b a90000 8f04707e 8f06
-0081:7fe0 707e8f 08707e 8f0a707e 8f0c707e a9ffff
-0081:7ff1 8f0e707e 6b  <- Ends at address 0081:7ff6
+0081:7F80 8A8D0205A93333CFF0707E
+0081:7F8B D0499C0270
+0081:7F90 AFF4707E1C0005AFF6707E2D00054A0C
+0081:7FA0 0270AFF8707E2D00050A0C0270AFFE70
+0081:7FB0 7E2D00050C0270AFFA707E2D00054A4A
+0081:7FC0 0C0270AFFC707E2D00050A0A0C0270AD
+0081:7FD0 02708D00056B6B6B6B
+
 
                 lda $7e7000
 		cmp #1A1A
