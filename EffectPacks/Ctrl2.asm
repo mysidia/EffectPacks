d4877179c7986c09eba380f7b095a06340e6a8e9
Mysidia <69487+mysidia@users.noreply.github.com>
2018-12-31 00:16:53 -0600

    working on sample usage description

--- EffectPacks/Ctrl2.asm
+++ EffectPacks/Ctrl2.asm
@@ -1,3 +1,42 @@
+; 65c816 opcodes reference - http://www.6502.org/tutorials/65c816opcodes.html#1
+;
+; // BV_COUNTER_BIT0 = 0x01
+; // BV_COUNTER_BIT1 = 0x02
+; // BV_COUNTER_BIT2 = 0x04
+; // BV_SHIFTRIGHT   = 0x00
+; // BV_SHIFTLEFT    = (1 << 4);
+; // BV_BYTESWAP     = (1 << 3);
+; result[0] = BV_MAGIC_1;     /* Enablement byte0 */
+; result[1] = BV_MAGIC_2;     /* Enablement byte1 */
+; if (abxy != PadState.NORMAL || dpad != PadState.NORMAL) {
+;     /* Turn off Select button while alternate controls in effect */
+;     result[10] = BV_DISABLE;                       /* Select button */
+; }
+; result[11] = 0;                                /* Start button */
+;
+; if  (abxy == PadState.INVERT) {  // Switched Button Controls
+;      /* Swap A and X buttons */
+;      result[4] = (BV_SHIFTRIGHT | BV_COUNTER_BIT0); /* A button */
+;      result[5] = (BV_SHIFTLEFT  | BV_COUNTER_BIT0); /* X button */
+;
+;      /* Swap L and R buttons */
+;      result[6] = (BV_SHIFTRIGHT | BV_COUNTER_BIT0); /* L button */
+;      result[7] = (BV_SHIFTLEFT  | BV_COUNTER_BIT0); /* R button */
+
+;      /* Swap B and Y buttons */
+;      result[8] = (BV_SHIFTRIGHT | BV_COUNTER_BIT0); /* B button */
+;      result[9] = (BV_SHIFTLEFT  | BV_COUNTER_BIT0); /* Y button */
+; }
+;
+; if  (dpad == PadState.INVERT) {  // Simple inversion
+;     /* Reverse both Dpad directions */
+;     result[12] = (BV_SHIFTRIGHT | BV_COUNTER_BIT0); /* Dpad Down */
+;     result[13] = (BV_SHIFTLEFT  | BV_COUNTER_BIT0); /* Dpad Up */
+;     result[14] = (BV_SHIFTRIGHT | BV_COUNTER_BIT0); /* Dpad Left */
+;     result[15] = (BV_SHIFTLEFT  | BV_COUNTER_BIT0); /* Dpad Right */
+; }
+; Connector?.WriteMemory(0x7e0000 + 0x70f0, result);
+
            STX   $1E74
            STA $0502 ;8D0205
            LDA #$3333  ;A93333
