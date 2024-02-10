2f4bf5d2b4b47021bcae8a2fcc0edff77d4be3c3
Mysidia <69487+mysidia@users.noreply.github.com>
2024-02-10 17:42:01 -0600

    rename cci to crowdinteract And multi-page output support

--- FindStreams/listcc-smw.py
+++ FindStreams/listcc-smw.py
@@ -5,6 +5,7 @@ import csv
 import os
 import re
 import time
+import urllib.parse
 
 import apptoken
 readTokens = apptoken.get_tokens()
@@ -12,7 +13,6 @@ client_id = readTokens[0]
 client_secret = readTokens[1]
 app_token = readTokens[2]
 gameid = 'game_id=1505160567&game_id=1229'
-
 
 #url1 = 'https://api.twitch.tv/helix/streams?first=100&'+str(gameid)
 #print("Lookup request:" + url1)
@@ -23,22 +23,59 @@ gameid = 'game_id=1505160567&game_id=1229'
 #channel_name = input('Enter the Twitch channel I:')  
 #lookup_request = requests.get('https://api.twitch.tv/helix/users?id=' + channel_name, headers = {'Client-ID' : client_id,  'Authorization' : 'Bearer ' +app_token})
 
+maximum_pagecount = 2
+current_page = 0
+first_page = True
+pagination_string = None
 
-lookup_request = requests.get('https://api.twitch.tv/helix/extensions/live?'+
-       'extension_id=7nydnrue11053qmjc6g0fd6einj75p',
-       headers = {'Client-ID' : client_id,  'Authorization' : 'Bearer ' +app_token})
+while first_page or (pagination_string and len(pagination_string) > 0):
+    first_page = False
+    current_page = current_page + 1
+    if current_page > maximum_pagecount:
+         break
+    if pagination_string:
+        pagination_query = f'&after={urllib.parse.quote(pagination_string)}'
+    else:
+        pagination_query = ''
+
+    lookup_url = ('https://api.twitch.tv/helix/extensions/live?'+
+          'extension_id=7nydnrue11053qmjc6g0fd6einj75p' + pagination_query)
+
+    print(f"requests.get(\"{lookup_url}\")")
+    lookup_request = requests.get(lookup_url,
+           headers = {'Client-ID' : client_id,  'Authorization' : 'Bearer ' +app_token})
+    pagination_string = None
+
+    if lookup_request.status_code == 200:
+        j1 = json.loads(lookup_request.text)
+        try:
+            for item in j1["data"]:
+                if 'broadcaster_id' in item and re.match(r'^\d+$', item["broadcaster_id"]  ):
+                    item["cclink"] = f'https://interact.crowdcontrol.live/#/twitch/{item["broadcaster_id"]}'
+            print(json.dumps(j1))
+            pagination_string = j1["pagination"]
+        except Exception as err:
+            raise Exception("Could not lookup " + str(err))
+        #print("SMW-Category:Viewers total=%s  streams=%s  median=%s" % (str(vtotal), str(scount), str(vmedian / 1) ))
+    else:
+        raise Exception("Could not lookup " + str(lookup_request.text))	
+    time.sleep(1)
+#
+
+ 
 
 
-if lookup_request.status_code == 200:
-    j1 = json.loads(lookup_request.text)
-    try:
-        for item in j1["data"]:
-            if 'broadcaster_id' in item and re.match(r'^\d+$', item["broadcaster_id"]  ):
-                item["cclink"] = f'https://interact.crowdcontrol.live/#/twitch/{item["broadcaster_id"]}'
-        print(json.dumps(j1))
-    except Exception as err:
-        raise Exception("Could not lookup " + str(err))
-    #print("SMW-Category:Viewers total=%s  streams=%s  median=%s" % (str(vtotal), str(scount), str(vmedian / 1) ))
-else:
-    raise Exception("Could not lookup " + str(lookup_request.text))	
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
